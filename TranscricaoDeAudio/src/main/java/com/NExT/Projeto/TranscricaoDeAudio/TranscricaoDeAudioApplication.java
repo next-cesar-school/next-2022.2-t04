91b3287f794cc858f9b92be456319e07e443ae6d
HenriSousa <Henrisousa7@hotmail.com>
2022-11-18 17:40:35 -0300

    Criação do Projeto
    Criação do projeto, com as Dependências SpringWeb e Cloud Translation.

---
+++ TranscricaoDeAudio/src/main/java/com/NExT/Projeto/TranscricaoDeAudio/TranscricaoDeAudioApplication.java
@@ -0,0 +1,13 @@
+package com.NExT.Projeto.TranscricaoDeAudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscricaoDeAudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscricaoDeAudioApplication.class, args);
+	}
+
+}
---
+++ TranscricaoDeAudio/src/test/java/com/NExT/Projeto/TranscricaoDeAudio/TranscricaoDeAudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NExT.Projeto.TranscricaoDeAudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscricaoDeAudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
