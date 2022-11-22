71c53e9519018bd3e5c2a35641cd3c4c9d922611
ilkafb <117782090+ilkafb@users.noreply.github.com>
2022-11-22 17:55:12 -0300

    Update app.java
    alteração de estrutura de decisão de if para switch e padronização de linguagem.

--- TranscricaoDeAudio/src/main/java/com/NExT/Projeto/TranscricaoDeAudio/app.java
+++ TranscricaoDeAudio/src/main/java/com/NExT/Projeto/TranscricaoDeAudio/app.java
@@ -7,13 +7,17 @@ import com.NExT.Projeto.TranscricaoDeAudio.service.Traduzir;
 public class app {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        Traduzir text = new Traduzir();
         System.out.println("Escolha o Idioma: PT = Português, EN = Inglês");
-        String escolha = sc.next();
+        String choice = sc.next();
 
-        if (escolha.equals(escolha)) {
-            Traduzir texto = new Traduzir();
+        switch (choice) {
+        
+        case "Pt":
+        
             System.out.println("Digite o texto que você quer traduzir do Português para o Inglês: ");
-            texto.PtToEn();
+            text.PtToEn();
+            break;
         }
 
         sc.close();
