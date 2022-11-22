9caa47c74fafe09a88cd79eacf7baa98238efa1e
JoseVictor22 <117787478+JoseVictor22@users.noreply.github.com>
2022-11-22 18:24:56 -0300

    Update app.java
    Adicionado as estruturas de decisão, utilizando o switch.

--- TranscricaoDeAudio/src/main/java/com/NExT/Projeto/TranscricaoDeAudio/app.java
+++ TranscricaoDeAudio/src/main/java/com/NExT/Projeto/TranscricaoDeAudio/app.java
@@ -18,6 +18,30 @@ public class app {
             System.out.println("Digite o texto que você quer traduzir do Português para o Inglês: ");
             text.PtToEn();
             break;
+
+        case "En":
+        
+            System.out.println("Digite o texto que você quer traduzir do Inglês para o Português: ");
+            text.EnToPt();
+            break;
+
+        case "DetectaPt":
+        
+            System.out.println("Digite o texto em qualquer idioma para traduzir em Português: ");
+            text.DetectaToPt();
+            break;
+
+        case "DetectaEn":
+        
+            System.out.println("Digite o texto em qualquer idioma para traduzir em Inglês: ");
+            text.DetectaToEn();
+            break;
+
+        case "DetectaEs":
+        
+            System.out.println("Digite o texto em qualquer idioma para traduzir em Espanhol: ");
+            text.DetectaToEs();
+            break;
         }
 
         sc.close();
